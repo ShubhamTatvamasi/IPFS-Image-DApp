1360eb3cc8dd691a20c94cf8ffa93177b05707fd
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-06-13 17:25:36 +0530

    Initial commit 	new file:   contract.js 	new file:   index.html 	new file:   main.js

---
+++ contract.js
@@ -0,0 +1,4 @@
+// IPFS contract address
+const IPFSAddress = "0x07860890564059D06E204a1D346CC31A45158980"
+// IPFS contract ABI
+const IPFSABI = [{"constant":true,"inputs":[],"name":"ipfsHash","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"x","type":"string"}],"name":"sendHash","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
---
+++ main.js
@@ -0,0 +1,62 @@
+// set the provider you want from Web3.providers
+web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io"))
+
+// IPFS Contract Interface
+const IPFSContract = web3.eth.contract(IPFSABI).at(IPFSAddress)
+
+const PublicKey = '0xbe862AD9AbFe6f22BCb087716c7D89a26051f74C'
+const PrivateKey = 'e331b6d69882b4cb4ea581d88e0b604039a3de5967688d3dcffdd2270c0fd109'
+var Nonce = 0
+
+// for getting the nonce from blockchain
+web3.eth.getTransactionCount(PublicKey, (error, result) => {
+  Nonce = result
+})
+
+IPFSContract.ipfsHash((error, result) => {
+    document.body.background = `https://ipfs.infura.io/ipfs/${result}`
+})
+
+function newBackground(hash) {
+
+  const data = IPFSContract.sendHash.getData(hash)
+
+  const tx = new ethereumjs.Tx({
+    nonce: Nonce,
+    gasPrice: 1e9,
+    gasLimit: 1e6,
+    to: IPFSAddress,
+    data: data
+  })
+
+  tx.sign(ethereumjs.Buffer.Buffer.from(PrivateKey, "hex"))
+
+  const raw = "0x" + tx.serialize().toString("hex")
+
+  web3.eth.sendRawTransaction(raw, (err, transactionHash) => {})
+
+  Nonce++
+
+}
+
+function upload() {
+  const reader = new FileReader();
+  reader.onloadend = function() {
+    const ipfs = window.IpfsApi({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) // Connect to IPFS
+    const buf = ethereumjs.Buffer.Buffer(reader.result) // Convert data into buffer
+    ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
+      if(err) {
+        console.error(err)
+        return
+      }
+      let url = `https://ipfs.infura.io/ipfs/${result[0].hash}`
+      console.log(`Url --> ${url}`)
+      document.body.background = url
+      newBackground(result[0].hash)
+    })
+  }
+  const photo = document.getElementById("photo");
+  reader.readAsArrayBuffer(photo.files[0]); // Read Provided File
+}
+document.getElementById("photo").addEventListener('input', upload);
+
